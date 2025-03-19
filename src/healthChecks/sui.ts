06cfabc65456088c8d0051c4e54e88590aa70a3f
Brian Li <bl@sm.xyz>
2025-03-19 13:54:27 +0900

    Add Sui validator endpoint.

--- src/healthChecks/sui.ts
+++ src/healthChecks/sui.ts
@@ -1,7 +1,7 @@
 import ky from "ky";
-
+import { Scheme } from "../types";
 export async function checkSuiRpcNode(
-  scheme: string,
+  scheme: Scheme,
   hostname: string
 ): Promise<number> {
   const response = await ky(`${scheme}://${hostname}/metrics`, {
@@ -15,16 +15,42 @@ export async function checkSuiRpcNode(
   const checkpointLine = response
     .split("\n")
     .find((line) => /^highest_synced_checkpoint\s\d+$/.test(line));
-  console.log(checkpointLine);
 
   if (!checkpointLine) {
     throw new Error("highest_synced_checkpoint metric not found");
   }
 
-  // Extract the number using regex
   const match = checkpointLine.match(/\d+$/);
   if (!match) {
     throw new Error("Could not extract checkpoint number");
+  }
+
+  return parseInt(match[0], 10);
+}
+
+export async function checkSuiValidatorNode(
+  scheme: Scheme,
+  hostname: string
+): Promise<number> {
+  const response = await ky(`${scheme}://${hostname}/metrics`, {
+    timeout: 5000,
+    retry: {
+      limit: 3,
+      methods: ["get"],
+    },
+  }).text();
+
+  const lastExecutedCheckpointTimestampLine = response
+    .split("\n")
+    .find((line) => /^last_executed_checkpoint_timestamp_ms\s\d+$/.test(line));
+
+  if (!lastExecutedCheckpointTimestampLine) {
+    throw new Error("highest_synced_checkpoint metric not found");
+  }
+
+  const match = lastExecutedCheckpointTimestampLine.match(/\d+$/);
+  if (!match) {
+    throw new Error("Could not extract timestamp.");
   }
 
   return parseInt(match[0], 10);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { checkSuiRpcNode } from "./healthChecks/sui";
+import { checkSuiRpcNode, checkSuiValidatorNode } from "./healthChecks/sui";
 import { checkWalrusStorageNode } from "./healthChecks/walrus";
 import { Scheme } from "./types";
 import { DEFAULT_INTERVAL } from "./constants";
@@ -19,6 +19,24 @@ app.get("/sui/rpc/:scheme/:hostname", async (c) => {
 
   if (check2 <= check1) {
     return c.text(`${hostname} is not syncing new checkpoints.`, 503);
+  }
+
+  return c.text("OK", 200);
+});
+
+app.get("/sui/validator/:scheme/:hostname", async (c) => {
+  const scheme = c.req.param("scheme") as Scheme;
+  const hostname = c.req.param("hostname") as string;
+  const interval = Number(c.req.query("interval")) || DEFAULT_INTERVAL;
+
+  const check1 = await checkSuiValidatorNode(scheme, hostname);
+  console.log(`${hostname} - ${check1}`);
+  await new Promise((resolve) => setTimeout(resolve, interval));
+  const check2 = await checkSuiValidatorNode(scheme, hostname);
+  console.log(`${hostname} - ${check2}`);
+
+  if (check2 <= check1) {
+    return c.text(`${hostname} is not persisting new events.`, 503);
   }
 
   return c.text("OK", 200);
