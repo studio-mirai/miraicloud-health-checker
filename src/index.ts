66b4318d705ac21bae90e7751d06c6069c36f189
Brian Li <bl@sm.xyz>
2025-03-19 13:47:35 +0900

    Cleanup.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
 import { Hono } from "hono";
 import { checkSuiRpcNode } from "./healthChecks/sui";
 import { checkWalrusStorageNode } from "./healthChecks/walrus";
+import { Scheme } from "./types";
+import { DEFAULT_INTERVAL } from "./constants";
 
 const app = new Hono();
-
-type Scheme = "http" | "https";
 
 app.get("/sui/rpc/:scheme/:hostname", async (c) => {
   const scheme = c.req.param("scheme") as Scheme;
   const hostname = c.req.param("hostname") as string;
-  const frequency = Number(c.req.query("frequency")) || 3_000;
+  const interval = Number(c.req.query("interval")) || DEFAULT_INTERVAL;
 
   const check1 = await checkSuiRpcNode(scheme, hostname);
   console.log(`${hostname} - ${check1}`);
-  await new Promise((resolve) => setTimeout(resolve, frequency));
+  await new Promise((resolve) => setTimeout(resolve, interval));
   const check2 = await checkSuiRpcNode(scheme, hostname);
   console.log(`${hostname} - ${check2}`);
 
@@ -27,11 +27,11 @@ app.get("/sui/rpc/:scheme/:hostname", async (c) => {
 app.get("/walrus/storage/:scheme/:hostname", async (c) => {
   const scheme = c.req.param("scheme") as Scheme;
   const hostname = c.req.param("hostname") as string;
-  const frequency = Number(c.req.query("frequency")) || 3_000;
+  const interval = Number(c.req.query("interval")) || DEFAULT_INTERVAL;
 
   const check1 = await checkWalrusStorageNode(scheme, hostname);
   console.log(`${hostname} - ${check1}`);
-  await new Promise((resolve) => setTimeout(resolve, frequency));
+  await new Promise((resolve) => setTimeout(resolve, interval));
   const check2 = await checkWalrusStorageNode(scheme, hostname);
   console.log(`${hostname} - ${check2}`);
 
